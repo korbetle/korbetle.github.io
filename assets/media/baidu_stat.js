e4562cde830374631aed156607826c2a2c98224c
Korben Zhang <korbenzhang@gmail.com>
2021-07-20 12:48:19 +0800

    new deploy

---
+++ assets/media/baidu_stat.js
@@ -0,0 +1,8 @@
+ar _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?f7ecd5cf2ee02b44ddd2545a9597e174";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
